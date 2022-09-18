da6f172a7433807031f575cc97cb93608cb74bba
Stark <serj.elessar@gmail.com>
2022-09-18 21:17:35 +0300

    fix: extended task JS with an updated webpack

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,17 +1,17 @@
 import webpack from 'webpack-stream'
+import { webpackConfig } from '../../webpack.config.js'
 
-export const js = () => {
-  return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
-    .pipe(app.plugins.plumber(
-      app.plugins.notify.onError({
-        title: 'JS',
-        message: 'Error: <%= error.message %>',
-      })
-    ))
-    .pipe(webpack({
-      mode: app.isBuild ? 'production' : 'development',
-      output: { filename: 'app.min.js' }
-    }))
+export const js = () =>
+  app.gulp
+    .src(app.path.src.js, { sourcemaps: app.isDev })
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: 'JS',
+          message: 'Error: <%= error.message %>',
+        })
+      )
+    )
+    .pipe(webpack({ config: webpackConfig(app.isDev) }))
     .pipe(app.gulp.dest(app.path.build.js))
     .pipe(app.plugins.browserSync.stream())
-}

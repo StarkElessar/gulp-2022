d21d33027288859c7bfef797555818e7785911b4
Stark <serj.elessar@gmail.com>
2022-09-18 21:58:15 +0300

    refactor: update

--- webpack.config.js
+++ webpack.config.js
@@ -8,35 +8,33 @@ const path = {
   build: pathNode.resolve(builFolder),
 }
 
-export const webpackConfig = (isMode) => {
-  return {
-    entry: ['@babel/polyfill', `${path.src}/js/app.js`],
-    mode: isMode ? 'development' : 'production',
-    cache: {  
-      type: 'filesystem', // По умолчанию 'memory'
-      cacheDirectory: `${path.src}/.temporary_cache`
-    }, 
-    output: {
-      path: `${path.build}/js`,
-      filename: 'app.min.js',
-      publicPath: '/',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-          resolve: {
-            fullySpecified: false,
+export const webpackConfig = (isMode) => ({
+  entry: ['@babel/polyfill', `${path.src}/js/app.js`],
+  mode: isMode ? 'development' : 'production',
+  cache: {
+    type: 'filesystem', // По умолчанию 'memory'
+    cacheDirectory: `${path.src}/.temporary_cache`,
+  },
+  output: {
+    path: `${path.build}/js`,
+    filename: 'app.min.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
         },
-      ],
-    },
-  }
-}
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+})

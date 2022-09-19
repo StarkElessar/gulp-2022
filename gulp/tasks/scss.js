b928dc8d13ebff097301f256e6d4d6e26dd80d45
Stark <serj.elessar@gmail.com>
2022-09-19 15:10:06 +0300

    refactor: clean

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -11,20 +11,17 @@ const sass = gulpSass(dartSass)
 
 export const scss = () => {
   return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
-    .pipe(app.plugins.plumber(
-      app.plugins.notify.onError({
-        title: 'SCSS',
-        message: 'Error: <%= error.message %>'
-      })))
+    .pipe(app.plugins.plumber(app.plugins.notify.onError({
+      title: 'SCSS',
+      message: 'Error: <%= error.message %>'
+    })))
     .pipe(app.plugins.replace(/@img\//g, '../images/'))
     .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(app.plugins.if(app.isBuild, groupCssMediaQueries()))
-    .pipe(app.plugins.if(app.isBuild, webpcss(
-      {
-        webpClass: '.webp',
-        noWebpClass: '.no-webp'
-      }
-    )))
+    .pipe(app.plugins.if(app.isBuild, webpcss({
+      webpClass: '.webp',
+      noWebpClass: '.no-webp'
+    })))
     .pipe(app.plugins.if(app.isBuild, autoPrefixer({
       grid: true,
       overrideBrowserslist: ['last 3 versions'],
@@ -37,5 +34,3 @@ export const scss = () => {
     .pipe(app.gulp.dest(app.path.build.css))
     .pipe(app.plugins.browserSync.stream())
 }
-
-// app.plugins.if(app.isBuild, )

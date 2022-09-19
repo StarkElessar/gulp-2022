f821f672756a13a2a37361bfe4f9409baf3536e0
Stark <serj.elessar@gmail.com>
2022-09-19 15:10:18 +0300

    fix: add htmlmin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,7 @@
 import fileinclude from "gulp-file-include"
 import webpHtmlNosvg from 'gulp-webp-html-nosvg'
 import versionNumber from 'gulp-version-number'
+import htmlMin from 'gulp-htmlmin'
 
 export const html = () => {
   return app.gulp.src(app.path.src.html)
@@ -11,6 +12,12 @@ export const html = () => {
       })
     ))
     .pipe(fileinclude())
+    .pipe(htmlMin({
+      useShortDoctype: true,
+      sortClassName: true,
+      collapseWhitespace: app.isBuild,
+      removeComments: app.isBuild
+    }))
     .pipe(app.plugins.replace(/@img\//g, 'images/'))
     .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
     .pipe(app.plugins.if(app.isBuild, versionNumber({

130eb2999db03847df5764c9cc178828fedea3b3
Stark <serj.elessar@gmail.com>
2022-12-01 09:45:38 +0300

    fix: снизил степень сжатия изображений

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -18,7 +18,7 @@ export const images = () => {
       progressive: true,
       svgoPlugins: [{ removeViewBox: false }],
       interlaced: true,
-      optimizationLevel: 4 // 0 to 7
+      optimizationLevel: 3 // 0 to 7
     })))
     .pipe(app.gulp.dest(app.path.build.images))
     .pipe(app.gulp.src(app.path.src.svg))

efd3116d2a33f11056a9a92ebc5e6a707b253ca6
Сергей <serj.elessar@gmail.com>
2022-12-17 12:35:00 +0300

    feat: добавил модуль загрузки сайта и добавления класса

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -33,9 +33,13 @@ function isWebp() {
     webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
   }
   // Добавление класса _webp или _no-webp для HTML 
-  testWebp(function (support) {
-    let className = support === true ? 'webp' : 'no-webp'
-    document.documentElement.classList.add(className)
+
+// Добавление loaded для HTML после полной загрузки страницы
+function addLoadedClass() {
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      html.classList.add('loaded')
+    }, 0)
   })
 }
 

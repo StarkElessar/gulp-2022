d699c52db7dbc03e1cfe6a287bee2d29aa80f74a
Сергей <serj.elessar@gmail.com>
2022-12-17 12:33:44 +0300

    refactor: обновил модуль для проверки поддержки webp

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -13,11 +13,23 @@ function FLS(message) {
 // Проверка браузера на поддержку .webp изображений =================================================================================================================
 function isWebp() {
   // Проверка поддержки webp
-  function testWebp(callback) {
-    let webP = new Image()
-    webP.onload = webP.onerror = function () {
-      callback(webP.height == 2)
-    }
+  const testWebp = (callback) => {
+    const webP = new Image()
+
+    webP.onload = webP.onerror = () => callback(webP.height === 2)
+    webP.src =
+      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+  }
+  // Добавление класса _webp или _no-webp для HTML
+  testWebp((support) => {
+    const className = support === true ? 'webp' : 'no-webp'
+    html.classList.add(className)
+
+    FLS(support === true ? 'webp поддерживается' : 'webp не поддерживается')
+  })
+}
+
+}
     webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
   }
   // Добавление класса _webp или _no-webp для HTML 

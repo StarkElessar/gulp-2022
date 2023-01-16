25357662d31aeabd3b82d7023f7e2a7bd9df829b
Сергей <serj.elessar@gmail.com>
2023-01-16 21:18:41 +0300

    refactor: навел порядок в коде

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,6 @@ import gulp from 'gulp' // Основной модуль
 import { path } from './gulp/config/path.js' // Импорт путей
 import { plugins } from './gulp/config/plugins.js' // Импорт общих плагинов
 
-// Передаем значения в глобальную переменную
-global.app = {
-  isBuild: process.argv.includes('--build'),
-  isDev: !process.argv.includes('--build'),
-  path: path,
-  gulp: gulp,
-  plugins: plugins,
-}
-
 // Импорт задач
 import { copy } from './gulp/tasks/copy.js'
 import { reset } from './gulp/tasks/reset.js'
@@ -24,6 +15,15 @@ import { svgSprive } from './gulp/tasks/svgSprive.js'
 import { zip } from './gulp/tasks/zip.js'
 import { ftp } from './gulp/tasks/ftp.js'
 
+// Передаем значения в глобальную переменную
+global.app = {
+  isBuild: process.argv.includes('--build'),
+  isDev: !process.argv.includes('--build'),
+  path,
+  gulp,
+  plugins,
+}
+
 // Наблюдатель за изменениями в файлах
 function watcher() {
   gulp.watch(path.watch.files, copy)
@@ -32,8 +32,6 @@ function watcher() {
   gulp.watch(path.watch.js, js)
   gulp.watch(path.watch.images, images)
 }
-
-export { svgSprive }
 
 // Последовательная обработка шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle)
@@ -45,11 +43,14 @@ const build = gulp.series(reset, mainTasks)
 const deployZIP = gulp.series(reset, mainTasks, zip)
 const deployFTP = gulp.series(reset, mainTasks, ftp)
 
-// Экспорт сценариев
-export { dev }
-export { build }
-export { deployZIP }
-export { deployFTP }
-
 // Выполнение сценария по умолчанию
 gulp.task('default', dev)
+
+// Экспорт сценариев
+export {
+  dev,
+  build,
+  deployZIP,
+  deployFTP,
+  svgSprive
+}

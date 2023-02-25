ec4b3fed1afcc0cdcfe82544f00e5e2894b74e4f
Сергей <serj.elessar@gmail.com>
2023-02-26 01:09:01 +0300

    fix: указал TS что элемент всегда будет присутствовать на странице

--- src/js/helpers/elementsNodeList.ts
+++ src/js/helpers/elementsNodeList.ts
@@ -1,6 +1,6 @@
 const html = document.documentElement as HTMLHtmlElement
 const body = document.body as HTMLBodyElement
-const pageWrapper = document.querySelector<HTMLDivElement>('.page')
+const pageWrapper = document.querySelector<HTMLDivElement>('.page')!
 const header = document.querySelector<HTMLElement>('.header')
 const firstScreen = document.querySelector<HTMLDivElement>('[data-observ]')
 const burgerButton = document.querySelector<HTMLButtonElement>('.icon-menu')

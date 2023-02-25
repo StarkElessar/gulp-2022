fc88dce3ef6b18f7f05fd0567d2115fbe55c462e
Сергей <serj.elessar@gmail.com>
2023-02-26 00:47:13 +0300

    fix: update comments

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ import { svgSprive } from './gulp/tasks/svgSprive.js'
 import { zip } from './gulp/tasks/zip.js'
 import { ftp } from './gulp/tasks/ftp.js'
 
-// Передаем значения в глобальную переменную
+/** Передаем значения в глобальную переменную */
 global.app = {
   isBuild: process.argv.includes('--build'),
   isDev: !process.argv.includes('--build'),
@@ -24,7 +24,7 @@ global.app = {
   plugins,
 }
 
-// Наблюдатель за изменениями в файлах
+/**  Наблюдатель за изменениями в файлах. */
 function watcher() {
   gulp.watch(path.watch.static, copy)
   gulp.watch(path.watch.html, html)
@@ -33,14 +33,22 @@ function watcher() {
   gulp.watch(path.watch.images, images)
 }
 
-// Последовательная обработка шрифтов
+/** Последовательная обработка шрифтов. */
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle)
-// Основные задачи
+
+/** Основные задачи. */
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images))
-// Построение сценариев выполнения задач
+
+/** Выполнение задач в режиме разработки. */
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
+
+/** Выполнение задач для боевой сборки проекта. */
 const build = gulp.series(reset, mainTasks)
+
+/** Выполнение задач для боевой сборки проекта и создания архива. */
 const deployZIP = gulp.series(reset, mainTasks, zip)
+
+/** Выполнение задач для отправки проекта по FTP-соединению. */
 const deployFTP = gulp.series(reset, mainTasks, ftp)
 
 // Выполнение сценария по умолчанию

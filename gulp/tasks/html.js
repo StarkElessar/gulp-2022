f1a8014e6de8e2559d8febeefc5df935d282849b
Stark <serj.elessar@gmail.com>
2023-04-25 18:28:38 +0300

    fix: merge master

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,7 +1,7 @@
-import webpHtml from 'gulp-webp-html-nosvg'
-import versionNumber from 'gulp-version-number'
-import htmlMin from 'gulp-htmlmin'
-import pug from 'gulp-pug'
+import webpHtml from 'gulp-webp-html-nosvg';
+import versionNumber from 'gulp-version-number';
+import htmlMin from 'gulp-htmlmin';
+import pug from 'gulp-pug';
 
 import data from '../../src/static/data.json' assert { type: 'json' };
 
@@ -27,26 +27,32 @@ const html = () => {
     )
     .pipe(app.plugins.replace(/@img\//g, 'images/'))
     .pipe(app.plugins.if(app.isBuild, webpHtml()))
-    .pipe(htmlMin({
-      useShortDoctype: true,
-      sortClassName: true,
-      collapseWhitespace: app.isBuild,
-      removeComments: app.isBuild
-    }))
-    .pipe(app.plugins.if(app.isBuild, versionNumber({
-      'value': '%DT%',
-      'append': {
-        'key': '_v',
-        'cover': 0,
-        'to': [
-          'css',
-          'js',
-        ],
-      },
-      'output': {
-        'file': 'gulp/version.json'
-      },
-    })))
+    .pipe(
+      htmlMin({
+        useShortDoctype: true,
+        sortClassName: true,
+        collapseWhitespace: app.isBuild,
+        removeComments: app.isBuild,
+      })
+    )
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        versionNumber({
+          value: '%DT%',
+          append: {
+            key: '_v',
+            cover: 0,
+            to: ['css', 'js'],
+          },
+          output: {
+            file: 'gulp/version.json',
+          },
+        })
+      )
+    )
     .pipe(app.gulp.dest(app.path.build.html))
-    .pipe(app.plugins.browserSync.stream())
-}
+    .pipe(app.plugins.browserSync.stream());
+};
+
+export { html };

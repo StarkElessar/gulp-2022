1914b3de71e506b05bfb719d1a1b1ef5f4896729
Stark <serj.elessar@gmail.com>
2023-05-12 19:53:42 +0300

    refactor: code style

--- webpack.config.js
+++ webpack.config.js
@@ -13,27 +13,33 @@ const paths = {
 
 export const webpackConfig = (isMode) => ({
   entry: ['@babel/polyfill', `${paths.src}/js/app.js`],
+
   mode: isMode ? 'development' : 'production',
+
   cache: {
     type: 'filesystem', // По умолчанию 'memory'
     cacheDirectory: `${paths.root}/.temporary_cache`,
   },
+
   output: {
     path: `${paths.build}/js`,
     filename: 'app.min.js',
     publicPath: '/',
   },
+
   module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
+
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
           },
         },
+
         resolve: {
           fullySpecified: false,
         },

b0c80432f429925bbade7ef020e1092c97518ddb
Stark <serj.elessar@gmail.com>
2023-06-20 00:08:23 +0300

    fix: update entry file

--- src/js/app.js
+++ src/js/app.js
@@ -2,63 +2,52 @@
  * !(i)
  * Код попадает в итоговый файл, только когда вызвана функция, например FLSFunctions.spollers();
  * Или когда импортирован весь файл, например import "files/script.js";
- * Неиспользуемый (не вызванный) код в итоговый файл не попадает.
+ * Неиспользуемый код в итоговый файл не попадает.
 
  * Если мы хотим добавить модуль следует его раскомментировать
  */
-import {
-  isWebp,
-  headerFixed,
-  togglePopupWindows,
-  addTouchClass,
-  addLoadedClass,
-} from './modules';
-
-// import BurgerMenu from './modules/BurgerMenu';
-
-// import Tabs from 'modules/Tabs';
-
-// import { MousePRLX } from './libs/parallaxMouse'
-
+// import MousePRLX from './libs/parallaxMouse'
 // import AOS from 'aos'
-
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
-/* Проверка поддержки webp, добавление класса webp или no-webp для HTML
- ! (i) необходимо для корректного отображения webp из css
- */
-isWebp();
+import BaseHelpers from './helpers/BaseHelpers.js';
+import PopupManager from './modules/PopupManager';
+import BurgerMenu from './modules/BurgerMenu';
+// import Tabs from 'modules/Tabs';
 
-/* Добавление класса touch для HTML если браузер мобильный */
-// addTouchClass();
+BaseHelpers.checkWebpSupport();
 
-/* Добавление loaded для HTML после полной загрузки страницы */
-// addLoadedClass();
+BaseHelpers.addTouchClass();
 
-/* Модуль для работы с меню (Бургер) */
-// new BurgerMenu().init();
+BaseHelpers.addLoadedClass();
+
+BaseHelpers.headerFixed();
+
+/**
+ * Открытие/закрытие модальных окон
+ * Чтобы модальное окно открывалось и закрывалось
+ * На окно повешай атрибут data-popup="<название окна>"
+ * На кнопку, которая вызывает окно повешай атрибут data-type="<название окна>"
+
+ * На обертку(.popup) окна добавь атрибут '[data-close-overlay]'
+ * На кнопку для закрытия окна добавь класс '.button-close'
+ * */
+new PopupManager();
+
+/**
+ *  Модуль для работы с меню (Бургер)
+ * */
+new BurgerMenu().init();
 
 /**
  *  Библиотека для анимаций
  *  документация: https://michalsnik.github.io/aos
- */
+ * */
 // AOS.init();
 
-/** Параллакс мышей */
-// const mousePrlx = new MousePRLX({});
-
-/** Фиксированный header */
-// headerFixed();
-
 /**
- *  Открытие/закрытие модальных окон
- * Чтобы модальное окно открывалось и закрывалось
- * На окно повешай атрибут data-popup="<название окна>"
- * И на кнопку, которая вызывает окно так же повешай атрибут data-type="<название окна>"
+ * Параллакс мышей
+ * */
+// new MousePRLX();
 
- * На обертку(враппер) окна добавь класс _overlay-bg
- * На кнопку для закрытия окна добавь класс button-close
- */
-// togglePopupWindows();
-
-// const tabs = new Tabs('default-tabs', {});
+// new Tabs('default-tabs', {});

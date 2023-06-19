0340172c2bbbd727e05a949e1b4f549fbe55972e
Stark <serj.elessar@gmail.com>
2023-06-19 20:20:01 +0300

    fix: update scss config

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -4,8 +4,10 @@ import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
 import cleanCss from 'gulp-clean-css'; // Сжатие CSS файла
 import webpCss from 'gulp-webpcss'; // Вывод WEBP изображений
-import autoPrefixer from 'gulp-autoprefixer'; // Добавление вендорных префиксов
-import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Группировка медиа запросов
+import groupMediaQueries from 'gulp-group-css-media-queries'; // Группировка медиа запросов
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import postcssPresetEnv from 'postcss-preset-env';
 
 import { filePaths } from '../config/paths.js';
 import { plugins } from '../config/plugins.js';
@@ -14,34 +16,28 @@ import { isBuild, isDev } from '../../gulpfile.js';
 const sass = gulpSass(dartSass);
 
 const scss = () => {
+  const webpConfig = {
+    webpClass: '.webp',
+    noWebpClass: '.no-webp',
+  };
+
   return (
     gulp
       .src(filePaths.src.scss, { sourcemaps: isDev })
       .pipe(plugins.handleError('SCSS'))
+
       .pipe(sass({ outputStyle: 'expanded' }))
       .pipe(plugins.replace(/@img\//g, '../images/'))
-      .pipe(plugins.if(isBuild, groupCssMediaQueries()))
-      .pipe(
-        plugins.if(
-          isBuild,
-          webpCss({
-            webpClass: '.webp',
-            noWebpClass: '.no-webp',
-          })
-        )
-      )
-      .pipe(
-        plugins.if(
-          isBuild,
-          autoPrefixer({
-            grid: true,
-            overrideBrowserslist: ['last 3 versions'],
-            cascade: true,
-          })
-        )
-      )
+
+      /** Группировка медиа-запросов только для production */
+      .pipe(plugins.if(isBuild, groupMediaQueries()))
+
+      .pipe(plugins.if(isBuild, webpCss(webpConfig)))
+      .pipe(plugins.if(isBuild, postcss([autoprefixer(), postcssPresetEnv()])))
+
       /** Раскомментировать если нужен не сжатый дубль файла стилей */
       // .pipe(gulp.dest(filePaths.build.css))
+
       .pipe(plugins.if(isBuild, cleanCss()))
       .pipe(rename({ extname: '.min.css' }))
       .pipe(gulp.dest(filePaths.build.css))

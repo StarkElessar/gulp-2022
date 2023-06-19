ec191978249adb3d0478e973ea03861f23d38f92
Stark <serj.elessar@gmail.com>
2023-06-19 21:21:12 +0300

    fix: remove global vars

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,12 @@ import { reset } from './gulp/tasks/reset.js';
 import { html } from './gulp/tasks/html.js';
 import { server } from './gulp/tasks/server.js';
 import { scss } from './gulp/tasks/scss.js';
-import { js } from './gulp/tasks/js.js';
+import { javaScript } from './gulp/tasks/javaScript.js';
 import { images } from './gulp/tasks/images.js';
 import { otfToTtf, ttfToWoff, fontStyle } from './gulp/tasks/fonts.js';
 import { createSvgSprite } from './gulp/tasks/createSvgSprite.js';
 import { zip } from './gulp/tasks/zip.js';
 import { ftp } from './gulp/tasks/ftp.js';
-
-// Передаем значения в глобальную переменную
-global.app = {
-  isBuild: process.argv.includes('--build'),
-  isDev: !process.argv.includes('--build'),
-  path: filePaths,
-  gulp,
-  plugins,
-};
 
 const isBuild = process.argv.includes('--build');
 const isDev = !process.argv.includes('--build');
@@ -37,26 +28,35 @@ function watcher() {
   gulp.watch(filePaths.watch.static, copy);
   gulp.watch(filePaths.watch.html, html);
   gulp.watch(filePaths.watch.scss, scss);
-  gulp.watch(filePaths.watch.js, js);
+  gulp.watch(filePaths.watch.js, javaScript);
   gulp.watch(filePaths.watch.images, images);
 }
 
-// Последовательная обработка шрифтов
+/**
+ * Последовательная обработка шрифтов
+ * */
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontStyle);
 
 /**
  * Параллельные задачи в режиме разработки
- */
-const devTasks = gulp.parallel(copy, copyRootFiles, html, scss, js, images);
+ * */
+const devTasks = gulp.parallel(
+  copy,
+  copyRootFiles,
+  html,
+  scss,
+  javaScript,
+  images
+);
 
 /**
  * Основные задачи
- */
+ * */
 const mainTasks = gulp.series(fonts, devTasks);
 
 /**
  * Построение сценариев выполнения задач
- */
+ * */
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
@@ -64,10 +64,10 @@ const deployFTP = gulp.series(reset, mainTasks, ftp);
 
 /**
  * Выполнение сценария по умолчанию
- */
+ * */
 gulp.task('default', dev);
 
 /**
  * Экспорт сценариев
- */
+ * */
 export { dev, build, deployZIP, deployFTP, createSvgSprite, isBuild, isDev };

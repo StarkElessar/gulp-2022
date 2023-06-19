adacabd2b4cbaa23239eb06c939d4a25a444d67d
Stark <serj.elessar@gmail.com>
2023-06-19 20:19:51 +0300

    fix: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,43 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const srcFolder = 'src';
-const buildFolder = 'dist';
+const __dirname = path.dirname(__filename);
 
 const paths = {
-  root: path.dirname(__filename),
-  src: path.resolve(srcFolder),
-  build: path.resolve(buildFolder),
+  src: path.resolve(__dirname, 'src'),
+  build: path.resolve(__dirname, 'dist'),
 };
 
-export const webpackConfig = (isMode) => ({
-  entry: ['@babel/polyfill', `${paths.src}/js/app.js`],
+export const webpackConfig = (isMode) => {
+  return {
+    entry: ['@babel/polyfill', path.join(paths.src, 'js/app.js')],
 
-  mode: isMode ? 'development' : 'production',
+    mode: isMode ? 'development' : 'production',
 
-  cache: {
-    type: 'filesystem', // По умолчанию 'memory'
-    cacheDirectory: `${paths.root}/.temporary_cache`,
-  },
+    output: {
+      path: path.join(paths.build, 'js'),
+      filename: 'app.min.js',
+      publicPath: '/',
+    },
 
-  output: {
-    path: `${paths.build}/js`,
-    filename: 'app.min.js',
-    publicPath: '/',
-  },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
 
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+          resolve: {
+            fullySpecified: false,
           },
         },
-
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-    ],
-  },
-});
+      ],
+    },
+  };
+};

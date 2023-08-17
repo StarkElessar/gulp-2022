6dce1aadbf992176bcfa5c8b2502b085a5c097ac
stark <serj.elessar@gmail.com>
2023-08-17 23:53:36 +0300

    fix: update ftp task

--- gulp/tasks/ftpDeploy.js
+++ gulp/tasks/ftpDeploy.js
@@ -1,19 +1,19 @@
 import gulp from 'gulp';
-import vinylFTP from 'vinyl-ftp';
+import ftp from 'vinyl-ftp';
 import util from 'gulp-util';
 
 import { configFTP } from '../config/ftp.js';
 import { filePaths } from '../config/paths.js';
 import { plugins } from '../config/plugins.js';
 
-const ftp = () => {
+const ftpDeploy = () => {
   configFTP.log = util.log;
-  const ftpConnect = vinylFTP.create(configFTP);
+  const ftpConnect = ftp.create(configFTP);
 
   return gulp
     .src(`${filePaths.buildFolder}/**/*.*`, {})
-    .pipe(plugins.handleError('FTP'))
+    .pipe(plugins.handleError('FTP_DEPLOY'))
     .pipe(ftpConnect.dest(`/${filePaths.ftp}/${filePaths.projectDirName}`));
 };
 
-export { ftp };
+export { ftpDeploy };

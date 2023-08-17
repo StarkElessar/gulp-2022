62d4131e3e7c2410fe5bb762a1abeccdde2aaa77
stark <serj.elessar@gmail.com>
2023-08-17 20:23:12 +0300

    fix: update gulpfile config

--- gulp/config/paths.js
+++ gulp/config/paths.js
@@ -1,6 +1,5 @@
 import path from 'path';
 
-const projectDirName = path.basename(path.resolve());
 const buildFolder = `./dist`;
 const srcFolder = `./src`;
 
@@ -8,7 +7,6 @@ const filePaths = {
   build: {
     js: `${buildFolder}/js/`,
     css: `${buildFolder}/css/`,
-    html: `${buildFolder}/`,
     images: `${buildFolder}/images/`,
     fonts: `${buildFolder}/fonts/`,
     static: `${buildFolder}/static/`,
@@ -17,7 +15,7 @@ const filePaths = {
     js: `${srcFolder}/js/app.js`,
     images: `${srcFolder}/images/**/*.{jpg,jpeg,png,gif,webp}`,
     svg: `${srcFolder}/images/**/*.svg`,
-    scss: `${srcFolder}/scss/style.scss`,
+    scss: `${srcFolder}/scss/main.scss`,
     html: `${srcFolder}/*.html`,
     static: `${srcFolder}/static/**/*.*`,
     svgIcons: `${srcFolder}/icons/*.svg`,
@@ -29,10 +27,9 @@ const filePaths = {
     images: `${srcFolder}/**/*.{jpg,jpeg,png,svg,gif,webp,ico}`,
     static: `${srcFolder}/static/**/*.*`,
   },
-  clean: buildFolder,
-  buildFolder: buildFolder,
-  srcFolder: srcFolder,
-  projectDirName,
+  buildFolder,
+  srcFolder,
+  projectDirName: path.basename(path.resolve()),
   ftp: ``, // Путь к нужной папке на удаленном сервере. Gulp добавит имя папки проекта автоматически
 };
 

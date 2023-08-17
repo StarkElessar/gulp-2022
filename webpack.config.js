6edef7d027dcecef3c1ede46f9414a58eda42231
stark <serj.elessar@gmail.com>
2023-08-17 21:57:01 +0300

    fix: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,13 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const paths = {
-  src: path.resolve(__dirname, 'src'),
-  build: path.resolve(__dirname, 'dist'),
+  src: path.resolve('src'),
+  build: path.resolve('dist'),
 };
 
 export const webpackConfig = (isMode) => {
   return {
-    entry: ['@babel/polyfill', path.join(paths.src, 'js/app.js')],
+    entry: path.join(paths.src, 'js/app.js'),
 
     mode: isMode ? 'development' : 'production',
 
@@ -28,10 +24,8 @@ export const webpackConfig = (isMode) => {
           exclude: /node_modules/,
 
           use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
+            loader: 'esbuild-loader',
+            options: { target: 'ES2015' },
           },
 
           resolve: {

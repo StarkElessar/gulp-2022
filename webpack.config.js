ae12db66dbbd748daee830ab9805bf934c159e0d
stark <serj.elessar@gmail.com>
2023-08-17 20:01:03 +0300

    fix: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,8 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const paths = {
-  src: path.resolve(__dirname, 'src'),
-  build: path.resolve(__dirname, 'dist'),
+  src: path.resolve('src'),
+  build: path.resolve('dist'),
 };
 
 export const webpackConfig = (isMode) => {

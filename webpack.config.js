bcc2f8de376b2e62a9f8f0d37bab3f4074fa3548
stark <serj.elessar@gmail.com>
2023-11-12 02:06:47 +0300

    fix: update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,34 @@
 import { resolve, join } from 'path';
+import { readDir } from './gulp/config/read-dir.js';
 
-export const webpackConfig = (isMode) => {
+export const webpackConfig = async (isMode) => {
   const paths = {
     src: resolve('src'),
     build: resolve('dist'),
   };
 
+	const context = join(paths.src, 'js');
+
   return {
-    context: join(paths.src, 'js'),
-
-    entry: {
-      main: './main.js',
-      test: './test.js'
-    },
-
+    context,
+    entry: await readDir(context),
     mode: isMode ? 'development' : 'production',
-
     output: {
       path: join(paths.build, 'js'),
       filename: '[name].min.js',
       publicPath: '/',
     },
-
     module: {
       rules: [
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
             },
           },
-
           resolve: {
             fullySpecified: false,
           },

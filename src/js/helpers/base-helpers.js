4e54fa91c662a0cd96e327d2b83b4aa1f3f5a9d8
stark <serj.elessar@gmail.com>
2024-05-05 15:28:36 +0300

    feat: добавил метод для сохранения ширины склолбара в css переменную

--- src/js/helpers/base-helpers.js
+++ src/js/helpers/base-helpers.js
@@ -1,74 +1,77 @@
-import MobileChecker from './mobile-checker';
+import { MobileChecker } from './mobile-checker';
 
-class BaseHelpers {
-  static html = document.documentElement;
-  static header = document.querySelector('.header');
-  static firstScreen = document.querySelector('[data-observ]');
+export class BaseHelpers {
+	static html = document.documentElement;
+	static header = document.querySelector('.header');
+	static firstScreen = document.querySelector('[data-observ]');
 
-  /**
-   * Проверка браузера на поддержку .webp изображений
-   * (i) необходимо для корректного отображения webp из css
-   * */
-  static checkWebpSupport() {
-    /** Проверка поддержки webp */
-    const testWebp = (callback) => {
-      const webP = new Image();
+	/**
+	 * Проверка браузера на поддержку .webp изображений
+	 * (i) необходимо для корректного отображения webp из css
+	 * */
+	static checkWebpSupport() {
+		/** Проверка поддержки webp */
+		const testWebp = (callback) => {
+			const webP = new Image();
 
-      webP.onload = webP.onerror = () => callback(webP.height === 2);
-      webP.src =
-        'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-    };
+			webP.onload = webP.onerror = () => callback(webP.height === 2);
+			webP.src =
+				'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+		};
 
-    /** Добавление класса _webp или _no-webp для HTML */
-    testWebp((support) => {
-      const className = support ? 'webp' : 'no-webp';
-	    BaseHelpers.html.classList.add(className);
+		/** Добавление класса _webp или _no-webp для HTML */
+		testWebp((support) => {
+			const className = support ? 'webp' : 'no-webp';
+			BaseHelpers.html.classList.add(className);
 
-      console.log(support ? 'webp поддерживается' : 'webp не поддерживается');
-    });
-  }
+			console.log(support ? 'webp поддерживается' : 'webp не поддерживается');
+		});
+	}
 
-  /**
-   * Добавление класса touch для HTML если браузер мобильный
-   * */
-  static addTouchClass() {
-    if (MobileChecker.isAny) {
-	    BaseHelpers.html.classList.add('touch');
-    }
-  }
+	/**
+	 * Добавление класса touch для HTML если браузер мобильный
+	 * */
+	static addTouchClass() {
+		if (MobileChecker.isAny) {
+			BaseHelpers.html.classList.add('touch');
+		}
+	}
 
-  /**
-   * Добавление loaded для HTML после полной загрузки страницы
-   * */
-  static addLoadedClass() {
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-	      BaseHelpers.html.classList.add('loaded');
-      }, 0);
-    });
-  }
+	/**
+	 * Добавление loaded для HTML после полной загрузки страницы
+	 * */
+	static addLoadedClass() {
+		window.addEventListener('load', () => {
+			setTimeout(() => {
+				BaseHelpers.html.classList.add('loaded');
+			}, 0);
+		});
+	}
 
-  /** Получение хеша в адресе сайта */
-  static get getHash() {
-    return location.hash?.replace('#', '');
-  }
+	/** Получение хеша в адресе сайта */
+	static get getHash() {
+		return location.hash?.replace('#', '');
+	}
 
-  /** Указание хеша в адресе сайта */
-  static setHash(hash) {
-    hash = hash ? `#${hash}` : location.href.split('#')[0];
-    history.pushState('', '', hash);
-  }
+	/** Указание хеша в адресе сайта */
+	static setHash(hash) {
+		hash = hash ? `#${hash}` : location.href.split('#')[0];
+		history.pushState('', '', hash);
+	}
 
-  /** Функция для фиксированной шапки при скролле */
-  static headerFixed() {
-    const headerStickyObserver = new IntersectionObserver(([entry]) => {
-	    BaseHelpers.html.classList.toggle('header-is-sticky', !entry.isIntersecting);
-    });
+	/** Функция для фиксированной шапки при скролле */
+	static headerFixed() {
+		const headerStickyObserver = new IntersectionObserver(([entry]) => {
+			BaseHelpers.html.classList.toggle('header-is-sticky', !entry.isIntersecting);
+		});
 
-    if (BaseHelpers.firstScreen) {
-      headerStickyObserver.observe(BaseHelpers.firstScreen);
-    }
-  }
+		if (BaseHelpers.firstScreen) {
+			headerStickyObserver.observe(BaseHelpers.firstScreen);
+		}
+	}
+
+	static calcScrollbarWidth() {
+		const scrollbarWidth = (window.innerWidth - document.body.clientWidth) / 16 + 'rem';
+		BaseHelpers.html.style.setProperty('--bh-scrollbar-width', scrollbarWidth);
+	}
 }
-
-export default BaseHelpers;
